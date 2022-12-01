ffa353b4e0995d6290a3022c5db38d1baf195df5
trecy <trecym@laposte.net>
2022-12-01 17:37:57 +0100

    page accueil et page à propos

---
+++ src/APropos.js
@@ -0,0 +1,6 @@
+import style from "./APropos.module.css";
+function APropos() {
+  return <h1 className={style["style-APropos"]}>Page À Propos</h1>;
+}
+
+export default APropos;
---
+++ src/Accueil.js
@@ -0,0 +1,5 @@
+import style from "./Accueil.module.css";
+function Accueil() {
+  return <h1 className={style["style-Accueil"]}>Page d'accueil</h1>;
+}
+export default Accueil;
