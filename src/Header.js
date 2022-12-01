3c02adc189093bbf050127d893055b70d3b7e35f
trecy <trecym@laposte.net>
2022-12-01 16:35:59 +0100

    mise en place de la navigation pour le header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,24 @@
 import logoHeader from "./logo.png";
 import style from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <img src={logoHeader} alt="Logo de l'enseigne" />
       <nav>
-        <li className={style["style-list"]}>Accueil</li>
-        <li className={style["style-list"]}>À propos</li>
+        {/* <Link to="Accueil">
+          <li className={style["style-list"]}>Accueil</li>
+        </Link>
+        <Link to="À propos">
+          <li className={style["style-list"]}>À propos</li>
+        </Link> */}
+        <Link to="Accueil" className={style["style-list"]}>
+          Accueil
+        </Link>
+        <Link to="À propos" className={style["style-list"]}>
+          À propos
+        </Link>
       </nav>
     </header>
   );
