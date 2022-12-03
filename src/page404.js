400858a5dfbb6c7c5d32766958abcc5d6fc0ad99
trecy <trecym@laposte.net>
2022-12-03 20:24:36 +0100

    rajout de texte dans le composant page404

--- src/page404.js
+++ src/page404.js
@@ -1,10 +1,17 @@
 import Header from "./Header";
 import style from "./page404.module.css";
+import { Link } from "react-router-dom";
 function Page404(params) {
   return (
     <>
       <Header />
-      <h1 className={style["style-Page404"]}>Page 404</h1>;
+      <h1 className={style["style-Page404"]}>404</h1>;
+      <p className={style["style-Page404"]}>
+        Oups!La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className={style["style-Page404"]}>
+        Retournez sur la page d'accueil
+      </Link>
     </>
   );
 }
